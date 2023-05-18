a741f9976c8ee18c20d314138c34e891035fd02d
gayoung <gayoungyeom@gmail.com>
2023-05-18 22:31:34 +0900

    3.3 count로 동작 확인

--- redux/app.js
+++ redux/app.js
@@ -5,14 +5,23 @@ function createStore() {
     state = worker(state);
   }
 
+  function getState() {
+    return state;
+  }
+
   return {
     send,
+    getState,
   };
 }
 
-function worker(state) {
-  return { ...state };
+function worker(state = { count: 0 }) {
+  return { ...state, count: state.count + 1 };
 }
 
 const store = createStore(worker);
+
 store.send();
+console.log(store.getState()); //{count: 1}
+store.send();
+console.log(store.getState()); //{count: 2}
